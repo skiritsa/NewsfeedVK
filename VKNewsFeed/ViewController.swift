9d2b80bc2bee0512232e2c55970c94b5573eb7d5
alexkiritsa <skiritsa@gmail.com>
2020-03-17 16:25:00 +0200

    Initial commit

---
+++ VKNewsFeed/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VKNewsFeed
+//
+//  Created by Alex Kiritsa on 17.03.2020.
+//  Copyright © 2020 Alex Kiritsa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
