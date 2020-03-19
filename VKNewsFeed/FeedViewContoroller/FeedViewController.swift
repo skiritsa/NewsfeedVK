103400291cf0d493f04dd4d58f5680b7f1b46f03
alexkiritsa <skiritsa@gmail.com>
2020-03-19 19:43:19 +0200

    Fetch News Feed JSON

--- VKNewsFeed/FeedViewContoroller/FeedViewController.swift
+++ VKNewsFeed/FeedViewContoroller/FeedViewController.swift
@@ -10,12 +10,20 @@ import UIKit
 
 class FeedViewController: UIViewController {
 
-    private let networkService = NetworkService()
+    private var fetcher: DataFetcher = NetworkDataFetcher(networking: NetworkService())
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         view.backgroundColor = #colorLiteral(red: 0.1764705926, green: 0.4980392158, blue: 0.7568627596, alpha: 1)
-        networkService.getFeed()
+        
+        fetcher.getFeed { (feedResponse) in
+            guard let feedResponse = feedResponse else { return }
+            feedResponse.items.map { (feedItem) in
+                print(feedItem.date)
+            }
+        }
+
         // Do any additional setup after loading the view.
     }
     
---
+++ VKNewsFeed/Services/Models/FeedResponse.swift
@@ -0,0 +1,32 @@
+//
+//  FeedResponse.swift
+//  VKNewsFeed
+//
+//  Created by Alex Kiritsa on 19.03.2020.
+//  Copyright © 2020 Alex Kiritsa. All rights reserved.
+//
+
+import Foundation
+
+struct FeedResponseWrapped: Decodable {
+    let response: FeedResponse
+}
+
+struct FeedResponse: Decodable {
+    var items: [FeedItem]
+}
+
+struct FeedItem: Decodable {
+    let sourceId: Int
+    let postId: Int
+    let text: String?
+    let date: Double
+    let comments: CountableItem?
+    let likes: CountableItem?
+    let reposts: CountableItem?
+    let views: CountableItem?
+}
+
+struct CountableItem: Decodable {
+    let count: Int
+}
