9dd437cf51c75114040bf37aaddce3b2c6fcb60e
alexkiritsa <skiritsa@gmail.com>
2020-03-29 18:19:50 +0300

    Fetch Groups and Users JSON Data

--- VKNewsFeed/Newsfeed/NewsfeedPresenter.swift
+++ VKNewsFeed/Newsfeed/NewsfeedPresenter.swift
@@ -20,6 +20,12 @@ protocol NewsfeedPresentationLogic
 class NewsfeedPresenter: NewsfeedPresentationLogic
 {
   weak var viewController: NewsfeedDisplayLogic?
+    let dateFormatter: DateFormatter = {
+        let dt = DateFormatter()
+        dt.locale = Locale(identifier: "ru_Ru")
+        dt.dateFormat = "d MMM 'в' HH:mm"
+        return dt
+    }()
   
   // MARK: Do something
   
@@ -29,21 +35,38 @@ class NewsfeedPresenter: NewsfeedPresentationLogic
     case .presentNewsfeed(let feed):
         
         let cells = feed.items.map { (feedItem) in
-            cellViewModel(from: feedItem)
+            cellViewModel(from: feedItem, profiles: feed.profiles, groups: feed.groups)
         }
         let feedViewModel = FeedViewModel.init(cells: cells)
         viewController?.displayData(viewModel: .displayNewfeed(feedViewMode: feedViewModel))
     }
   }
     
-    private func cellViewModel(from feedItem: FeedItem) -> FeedViewModel.Cell {
-        return FeedViewModel.Cell.init(iconUrlString: "",
-                                       name: "future name",
-                                       date: "future date",
+    private func cellViewModel(from feedItem: FeedItem, profiles: [Profile], groups: [Group]) -> FeedViewModel.Cell {
+        
+        let profile = self.profile(for: feedItem.sourceId, profiles: profiles, groups: groups)
+        
+        let date = Date(timeIntervalSince1970: feedItem.date)
+        let dateTitle = dateFormatter.string(from: date)
+        
+        return FeedViewModel.Cell.init(iconUrlString: profile.photo,
+                                       name:  profile.name,
+                                       date: dateTitle,
                                        text: feedItem.text,
                                        likes: String(feedItem.views?.count ?? 0),
                                        comments: String(feedItem.views?.count ?? 0),
                                        shares: String(feedItem.views?.count ?? 0),
                                        views: String(feedItem.views?.count ?? 0))
+    }
+    
+    private func profile( for sourseId: Int, profiles: [Profile], groups: [Group]) -> ProfileRepresenatable {
+     
+        let profilesOrGroups: [ProfileRepresenatable] = sourseId >= 0 ? profiles : groups
+        let normalSourseId = sourseId >= 0 ? sourseId : -sourseId
+        let profileRepresenatable = profilesOrGroups.first { (myProfileRepresenatable) -> Bool in
+            myProfileRepresenatable.id == normalSourseId
+        }
+        
+        return profileRepresenatable!
     }
 }
